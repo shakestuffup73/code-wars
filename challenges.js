6fe75c1d2e39731f581a2e3ed232f130f07f01ae
Amanda Stern <amanda.stern.73@gmail.com>
2022-12-18 17:11:22 -0500

    complete challenge 8

--- challenges.js
+++ challenges.js
@@ -149,3 +149,39 @@ for (let i = 0; i <= 100; i++){
     console.log(i)
   }
 }
+
+// Challenge Problem 8: Highest Scoring Word
+
+// letterValues = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26 }
+
+function high(string){
+  // each letter scores points
+  // a=1, b=2, c=3...to z=26
+  // add letters in each word in the string
+  let array = string.split(' ')
+  console.log(array)
+  
+  let scoreArray = []
+  
+  array.forEach((word)=> {
+    console.log('this is word', word)
+    let wordArray = word.split('')
+    console.log('this is wordArray', wordArray)
+    
+    let wordTotal = 0
+    
+    for (let i = 0; i < wordArray.length; i++) {
+      console.log('this is wordArray[i]', wordArray[i])
+      wordTotal += (wordArray[i].charCodeAt()-96)
+      console.log('this is wordTotal', wordTotal)
+    }
+    scoreArray.push(wordTotal)
+    console.log('this is scoreArray', scoreArray)
+  })
+  // return word with highest value
+  // if tie, return first word with highest tied value
+  let highestVal = scoreArray.indexOf(Math.max(...scoreArray))
+  return array[highestVal]
+}
+
+console.log(high('this is bananas'))
